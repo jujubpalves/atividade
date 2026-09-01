10d9dd3abcfd70354fb261ebe11be62961a2baaa
56576890893 <juju.bpalves@gmail.com>
2026-09-01 14:44:59 -0300

    feat: add exercicio 4

--- src/main/java/MainJava.java
+++ src/main/java/MainJava.java
@@ -1,16 +1,22 @@
 package org.example;
 import java.util.Scanner;
 
-// Exercício 1
+// Exercício 4
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nota = sc.nextDouble();
 
 
-        if (nota >= 7 ){
-            System.out.println("Aprovado");
-        } else {
-            System.out.println("Reprovado");
+        if (nota >= 9 ){
+            System.out.println("Excelente");
+        } else if(nota <= 7) {
+            System.out.println("Bom");
+        }
+
+        if (nota >= 5 ){
+            System.out.println("Regular");
+        } else  {
+            System.out.println("Insuficiente");
         }
     }
