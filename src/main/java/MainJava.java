12f7abda42ff8baff06b1d4188f4543887c48c51
56576890893 <juju.bpalves@gmail.com>
2026-09-01 14:08:49 -0300

    feat: add exercicio 2

--- src/main/java/MainJava.java
+++ src/main/java/MainJava.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int idade = sc.nextInt();
+        double nota = sc.nextDouble();
 
 
-        if (idade >=18 ){
-            System.out.println("Maior de idade");
+        if (nota >= 7 ){
+            System.out.println("Aprovado");
         } else {
-            System.out.println("Menor de idade");
+            System.out.println("Reprovado");
         }
     }
