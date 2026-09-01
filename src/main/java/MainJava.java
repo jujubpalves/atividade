1afc90d37f0c636bb83ed3e9d4b387fc7f6d9337
56576890893 <juju.bpalves@gmail.com>
2026-09-01 14:37:29 -0300

    feat: add exercicio 3

--- src/main/java/MainJava.java
+++ src/main/java/MainJava.java
@@ -1,7 +1,7 @@
 package org.example;
 import java.util.Scanner;
 
-// Exercício 1:
+// Exercício 1
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
